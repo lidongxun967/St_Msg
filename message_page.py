8fd34f31aca92ded7f1ea48860ca3faf44bdba8f
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-04-14 13:02:20 +0800

    Update message_page.py

--- message_page.py
+++ message_page.py
@@ -12,13 +12,15 @@ txt = st.text_area("输入你的留言")
 
 if isV(emt) and txt:
   et = st.button('发送')
-else:
+elif not isV(emt):
   st.toast("无效的邮件地址！",icon="❌")
   et = st.button('发送',disabled=True)
+elif not txt:
+  st.toast("留言内容空！",icon="❌")
 
 
 if et: #APIKEY在Streamlit Secrets中提供
   evc = Thev.EVCode('smtp.office365.com','debug967login@outlook.com',st.secrets["APIKEY"])
   c=evc.send_verification_code('1985409711@qq.com',emt,txt)
   st.text('留言已发送！')
-  
+  st.success('留言已发送！', icon="📨")
